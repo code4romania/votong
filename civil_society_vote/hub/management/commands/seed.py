cdb3b822a2fd15b5e1c3ebf4b0adebf08b2b1bb2
Angel Ramboi <angel@ramboi.com>
2020-05-23 18:30:55 +0200

    Updates to the seeding script

--- civil_society_vote/hub/management/commands/seed.py
+++ civil_society_vote/hub/management/commands/seed.py
@@ -6,6 +6,7 @@ from faker import Faker
 from hub.models import (
     City,
     Organization,
+    Candidate,
     OrganizationVote,
     ADMIN_GROUP_NAME,
     NGO_GROUP_NAME,
@@ -15,30 +16,36 @@ from hub.models import (
 
 fake = Faker()
 
-ORGS = [
-    {
-        "name": fake.company(),
-        "email": fake.email(),
-        "phone": fake.phone_number(),
-        "address": fake.address(),
-        "domain": ((i % 6) + 1),
-        "reg_com_number": fake.ssn(),
-        "purpose_initial": fake.sentence(),
-        "purpose_current": fake.sentence(),
-        "founders": fake.name(),
-        "representative": fake.name(),
-        "board_council": fake.name(),
-        "city": ["Arad", "Timisoara", "Oradea", "Cluj-Napoca", "Bucuresti"][i % 5],
-        "county": ["Arad", "Timis", "Bihor", "Cluj", "Bucuresti"][i % 5],
-        "status": ["pending", "accepted", "rejected"][i % 3],
-    }
-    for i in range(30)
-]
+ORG_NUMBER = 30
 
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         self.stdout.write(self.style.NOTICE("Starting the seeding process"))
+
+        orgs = [
+            {
+                "name": fake.company(),
+                "email": fake.safe_email(),
+                "phone": fake.phone_number(),
+                "address": fake.address(),
+                "domain": ((i % 6) + 1),
+                "reg_com_number": fake.ssn(),
+                "purpose_initial": fake.sentence(),
+                "purpose_current": fake.sentence(),
+                "founders": fake.name(),
+                "representative": fake.name(),
+                "board_council": fake.name(),
+                "city": ["Arad", "Timisoara", "Oradea", "Cluj-Napoca", "Bucuresti"][i % 5],
+                "county": ["Arad", "Timis", "Bihor", "Cluj", "Bucuresti"][i % 5],
+                "status": ["pending", "accepted", "rejected"][i % 3],
+                "logo": "/static/images/logo-demo.png",
+                "last_balance_sheet": "/static/data/test.pdf",
+                "statute": "/static/data/test.pdf",
+                "letter": "/static/data/test.pdf",
+            }
+            for i in range(ORG_NUMBER)
+        ]
 
         if not User.objects.filter(username="admin").exists():
             User.objects.create_user("admin", "admin@example.test", "secret", is_staff=True, is_superuser=True)
@@ -108,6 +115,8 @@ class Command(BaseCommand):
             {"city": "Timisoara", "county": "Timis"},
             {"city": "Oradea", "county": "Bihor"},
             {"city": "Cluj-Napoca", "county": "Cluj"},
+            {"city": "Constanta", "county": "Constanta"},
+            {"city": "Iasi", "county": "Iasi"},
             {"city": "Bucuresti", "county": "Bucuresti"},
         ]
         for city_data in cities:
@@ -115,11 +124,28 @@ class Command(BaseCommand):
 
         self.stdout.write(self.style.SUCCESS("Loaded city data"))
 
-        for details in ORGS:
-            details["city"] = City.objects.get(city=details["city"], county=details["county"])
-            details["county"] = details["county"]
-
-            org, _ = Organization.objects.get_or_create(**details)
+        for i in range(ORG_NUMBER):
+            city = City.objects.order_by('?').first()
+            org = Organization.objects.create(
+                name=fake.company(),
+                email=fake.safe_email(),
+                phone=fake.phone_number(),
+                address=fake.address(),
+                domain=((i % 6) + 1),
+                reg_com_number=fake.ssn(),
+                purpose_initial=fake.sentence(),
+                purpose_current=fake.sentence(),
+                founders=fake.name(),
+                representative=fake.name(),
+                board_council=fake.name(),
+                city=city,
+                county=city.county,
+                status=["pending", "accepted", "rejected"][i % 3],
+                logo="../static/images/logo-demo.png",
+                last_balance_sheet="../static/data/test.pdf",
+                statute="../static/data/test.pdf",
+                letter="../static/data/test.pdf",
+            )
 
             owner = User.objects.create_user(
                 fake.user_name(),
@@ -134,7 +160,25 @@ class Command(BaseCommand):
             org.user = owner
             org.save()
 
-            self.stdout.write(self.style.SUCCESS(f"Created organization: {org}"))
+            candidate = Candidate.objects.create(
+                org=org,
+                name=org.representative,
+                role=fake.job(),
+                experience=fake.text(),
+                studies=fake.text(),
+                representative=True,
+                email=fake.safe_email(),
+                phone=fake.phone_number(),
+                domain=org.domain,
+                photo="../static/images/photo-placeholder.gif",
+                mandate="../static/data/test.pdf",
+                letter="../static/data/test.pdf",
+                statement="../static/data/test.pdf",
+                cv="../static/data/test.pdf",
+                legal_record="../static/data/test.pdf",
+            )
+
+            self.stdout.write(self.style.SUCCESS(f"Created organization {org} with candidate {candidate.name}"))
 
         self.stdout.write(self.style.SUCCESS("Loaded organizations data"))
 
