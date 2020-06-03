99b70160e48351956df95b94d5ea98449a2baec8
Angel Ramboi <angel@ramboi.com>
2020-06-03 10:26:48 +0200

    Fix black

--- civil_society_vote/hub/management/commands/seed.py
+++ civil_society_vote/hub/management/commands/seed.py
@@ -105,7 +105,10 @@ class Command(BaseCommand):
             domains = [
                 {"name": "Organizații academice și profesionale", "description": fake.text()},
                 {"name": "Organizații cooperatiste și agricole", "description": fake.text()},
-                {"name": "Organizații din domeniul social, familia și persoane cu dizabiltăți și pensionari", "description": fake.text()},
+                {
+                    "name": "Organizații din domeniul social, familia și persoane cu dizabiltăți și pensionari",
+                    "description": fake.text(),
+                },
                 {"name": "Organizații din domeniul sănătății/educației", "description": fake.text()},
                 {"name": "Organizații din domeniul mediului", "description": fake.text()},
                 {"name": "Organizații pentru Protecția Drepturilor Omului", "description": fake.text()},
