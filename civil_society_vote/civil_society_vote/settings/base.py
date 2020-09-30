129249a058a8bfeb9196c3091a0f2ff5c9f4cd2a
Vlad Temian <vladtemian@gmail.com>
2020-09-30 19:21:44 +0300

    Add a custom user model that has email as username (#56)

--- civil_society_vote/civil_society_vote/settings/base.py
+++ civil_society_vote/civil_society_vote/settings/base.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     # apps
     "hub",
+    "accounts",
     # third-party
     "admin_auto_filters",
     "spurl",
@@ -116,6 +117,7 @@ AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "hub.password_validation.PasswordDifferentFromPrevious"},
 ]
 
+AUTH_USER_MODEL = "accounts.User"
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
