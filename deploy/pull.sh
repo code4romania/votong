f60f46fbf0fa77a2a9c24431faa079e37be1a1b2
Vlad Temian <vladtemian@gmail.com>
2020-10-14 13:05:11 +0300

    Use dirname to handle code source deploys

--- deploy/pull.sh
+++ deploy/pull.sh
@@ -1,7 +1,10 @@
 #! /usr/bin/env sh
 set -x
 
-cd $VOTONG_HOME \
+DEPLOY_DIR=$(dirname "$0")
+SOURCE_DIR="${DEPLOY_DIR}/../"
+
+cd $SOURCE_DIR \
     && git pull \
     && docker-compose down \
     && docker-compose build \
