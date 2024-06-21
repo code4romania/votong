0265308352aa91532a2d2622d94e75d539441e0b
Daniel Ursache Dogariu <contact@danniel.net>
2024-06-21 15:39:12 +0300

    Automatically assign social auth users to the NGO group

--- backend/hub/social_adapters.py
+++ backend/hub/social_adapters.py
@@ -1,11 +1,16 @@
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
+from django.contrib.auth.models import Group
 
-from .models import Organization
+from .models import Organization, NGO_GROUP
 
 
 class UserOrgAdapter(DefaultSocialAccountAdapter):
     def save_user(self, request, sociallogin, form=None):
         user = super().save_user(request, sociallogin, form)
+
+        # Add the user to the NGO group
+        ngo_group = Group.objects.get(name=NGO_GROUP)
+        user.groups.add(ngo_group)
 
         # Create a blank Organization for the newly registered User
         org = Organization(user=user, status=Organization.STATUS.accepted)
