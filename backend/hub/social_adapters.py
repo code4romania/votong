3e31399338eca83a492ddee4b9e7335064502798
Daniel Ursache Dogariu <contact@danniel.net>
2024-07-02 17:35:08 +0300

    Save some org data from NGO Hub on login

--- backend/hub/social_adapters.py
+++ backend/hub/social_adapters.py
@@ -2,6 +2,7 @@ import requests
 
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
 from allauth.socialaccount.signals import pre_social_login, social_account_updated
+# from allauth.socialaccount.models import SocialToken, SocialAccount
 from django.contrib.auth.models import Group
 from django.conf import settings
 
@@ -17,7 +18,7 @@ class UserOrgAdapter(DefaultSocialAccountAdapter):
         user.groups.add(ngo_group)
 
         # Create a blank Organization for the newly registered User
-        org = Organization(user=user, status=Organization.STATUS.accepted)
+        org = Organization(user=user, accept_terms_and_conditions=True, status=Organization.STATUS.accepted)
         org.save()
 
         return user
@@ -31,14 +32,36 @@ def update_user_org(sender, **kwargs):
 
     # print("code =", request.GET.get("code"))
     # print("user =", social.user)
-    # print("token =", social.token)
+    print("token =", social.token)
+
     auth_headers = {"Authorization": f"Bearer {social.token}"}
+    # response = requests.get(settings.NGOHUB_API_BASE + "api/ong-user/", headers=auth_headers)
+    # print(response.json())
 
-    response = requests.get(settings.NGOHUB_API_BASE + "api/ong-user/", headers=auth_headers)
-    print(response.json())
+    ngohub_org = requests.get(settings.NGOHUB_API_BASE + "organization-profile/", headers=auth_headers).json()
+    print(ngohub_org)
+    
+    org = Organization.objects.filter(user=social.user)[0]
+    org.ngohub_org_id = ngohub_org["id"]
+    org.name = ngohub_org["organizationGeneral"]["name"]
+    # org.county = ""
+    # org.city = ""
+    org.address = ngohub_org["organizationGeneral"]["address"]
+    org.registration_number = ngohub_org["organizationGeneral"]["cui"]
 
-    response = requests.get(settings.NGOHUB_API_BASE + "organization-profile/", headers=auth_headers)
-    print(response.json())
+    org.email = ngohub_org["organizationGeneral"]["contact"]["email"]
+    org.phone = ngohub_org["organizationGeneral"]["contact"]["phone"]
+    org.description = ngohub_org["organizationGeneral"]["description"]
+
+    org.legal_representative_name = ngohub_org["organizationLegal"]["legalReprezentative"]["fullName"]
+    org.legal_representative_email = ngohub_org["organizationLegal"]["legalReprezentative"]["email"]
+    org.legal_representative_phone = ngohub_org["organizationLegal"]["legalReprezentative"]["phone"]
+
+    # org.organisation_head_name = ""
+    # org.board_council = ""
+
+    org.save()
+    
 
 
 social_account_updated.connect(update_user_org)
