8b0d6072e0e1c3e9e5a147fc33c5f18b804d2217
Daniel Ursache Dogariu <contact@danniel.net>
2024-07-11 13:47:51 +0300

    Update the template context processors

--- backend/hub/context_processors.py
+++ backend/hub/context_processors.py
@@ -7,14 +7,17 @@ def hub_settings(context):
     flags = {k: v for k, v in FeatureFlag.objects.all().values_list("flag", "is_enabled")}
 
     return {
-        "DEBUG": settings.DEBUG,
-        "CURRENT_EDITION_YEAR": settings.CURRENT_EDITION_YEAR,
+        # Flags from settings.py:
         "ANALYTICS_ENABLED": settings.ANALYTICS_ENABLED,
+        "CURRENT_EDITION_TYPE": settings.CURRENT_EDITION_TYPE,
+        "CURRENT_EDITION_YEAR": settings.CURRENT_EDITION_YEAR,
+        "DEBUG": settings.DEBUG,
         "ENABLE_ORG_REGISTRATION_FORM": settings.ENABLE_ORG_REGISTRATION_FORM,
-        "GLOBAL_SUPPORT_ENABLED": flags.get(FLAG_CHOICES.global_support_round, False),
-        "ORG_REGISTRATION_ENABLED": flags.get(FLAG_CHOICES.enable_org_registration, False),
-        "ORG_APPROVAL_ENABLED": flags.get(FLAG_CHOICES.enable_org_approval, False),
+        # Flags from database:
         "CANDIDATE_REGISTRATION_ENABLED": flags.get(FLAG_CHOICES.enable_candidate_registration, False),
         "CANDIDATE_SUPPORTING_ENABLED": flags.get(FLAG_CHOICES.enable_candidate_supporting, False),
         "CANDIDATE_VOTING_ENABLED": flags.get(FLAG_CHOICES.enable_candidate_voting, False),
+        "GLOBAL_SUPPORT_ENABLED": flags.get(FLAG_CHOICES.global_support_round, False),
+        "ORG_APPROVAL_ENABLED": flags.get(FLAG_CHOICES.enable_org_approval, False),
+        "ORG_REGISTRATION_ENABLED": flags.get(FLAG_CHOICES.enable_org_registration, False),
     }
